1f63a95ad60fc53aa62dffce85213c6588c21295
Alfiami <alfiamisholihatun@gmail.com>
2024-04-08 13:26:23 +0700

    add folder

--- Jobsheet-3/lib/main.dart
+++ Jobsheet-3/lib/main.dart
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 51, 38, 42)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 51, 38, 42)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Hello Alfiami Sholihatun'),
